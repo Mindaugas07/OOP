5837d5695f3085b661fb7168f6c7170c37eb8b9b
Mindaugas Minelga <minelga.m@gmail.com>
2023-11-08 18:56:37 +0200

    add main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+# def two_number_division(number_one: int, number_two: int) -> float:
+#     try:
+#         return number_one / number_two
+#     except Exception as err:
+#         print(f"You got {err} error!")
+
+# def betkas(number_one: int, number_two: int) -> float:
+#     answer_number = two_number_division(number_one, number_two)
+#     if not answer_number:
+#         return 0    
+#     return answer_number ** 2
+
+
+
+# # two_number_division(4, 0)
+# print(betkas(4, 0))
